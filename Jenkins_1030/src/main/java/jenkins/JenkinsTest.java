93f21c85132ffa7a2ddae02a007b4b54a9dd123e
D VIJAYA LAKSHMI <D VIJAYA LAKSHMI@LAPTOP-576F71HH>
2024-08-15 11:59:05 +0530

    jenkins launch

---
+++ Jenkins_1030/src/main/java/jenkins/JenkinsTest.java
@@ -0,0 +1,26 @@
+package jenkins;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest
+{
+	@Test
+	public void  launch()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("demo web shop launched",true);
+	}
+	{
+		
+	}
+
+}
