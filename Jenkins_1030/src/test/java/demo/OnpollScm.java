957020259e84f29c2939ff4296d3cd49a30272f3
D VIJAYA LAKSHMI <D VIJAYA LAKSHMI@LAPTOP-576F71HH>
2024-08-16 11:53:53 +0530

    scm poll

---
+++ Jenkins_1030/src/test/java/demo/OnpollScm.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnpollScm 
+{
+	@Test
+	public void onpoll()
+	{
+		Reporter.log("on poll launched successfully");
+	}
+
+}
